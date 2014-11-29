1e1c2aa5f1c6bb4d8801252a8a3e6d3177106e76
Arie <prasetyo.mailbox@gmail.com>
2014-11-29 07:15:13 +0700

    update

---
+++ melon/view/center/mokletdevHomeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  mokletdevHomeTableViewCell.h
+//  langitmusik
+//
+//  Created by Arie Prasetyo on 11/28/14.
+//  Copyright (c) 2014 mokletdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mokletdevHomeTableViewCell : UITableViewCell
+
+@end
---
+++ melon/view/center/mokletdevHomeTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  mokletdevHomeTableViewCell.m
+//  langitmusik
+//
+//  Created by Arie Prasetyo on 11/28/14.
+//  Copyright (c) 2014 mokletdev. All rights reserved.
+//
+
+#import "mokletdevHomeTableViewCell.h"
+
+@implementation mokletdevHomeTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
